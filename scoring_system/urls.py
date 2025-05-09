0db33d236dc773b5dd7bc6c57d2f55bb7edb7023
tslab-fukuda <fukuda.takumi@nihon-u.ac.jp>
2025-05-09 04:52:05 +0000

    ログイン画面への遷移URL追記

--- scoring_system/urls.py
+++ scoring_system/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('submission/', include('submission.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
