8512d74c434fb900b16add2860220d92b9f344f1
tslab-fukuda <fukuda.takumi@nihon-u.ac.jp>
2025-05-22 09:56:37 +0000

    headerでロールが参照できていなかった問題を修正

--- submission/static/submission/js/header.js
+++ submission/static/submission/js/header.js
@@ -2,11 +2,11 @@ new Vue({
     el: '#vue-header-app',
     data: {
       showMenu: false,
-      role: window.USER_ROLE || 'student',
-      userName: window.USER_NAME || 'USER'
+      role: USER_ROLE || 'student',
+      userName: USER_NAME || 'USER'
     },
     methods: {
       toggleMenu() { this.showMenu = !this.showMenu; },
-      closeMenu() { this.showMenu = false; }
+      closeMenu() { this.showMenu = false; },
     }
   });
