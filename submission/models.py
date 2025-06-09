e2e48eab81b1c80285cdc14350196dca73fc9dd2
tslab-fukuda <fukuda.takumi@nihon-u.ac.jp>
2025-06-09 07:25:14 +0000

    migrate後ファイル

--- submission/models.py
+++ submission/models.py
@@ -49,7 +49,7 @@ class UserProfile(models.Model):
         ('火', '火'), ('木', '木')
     ])
     experiment_group = models.CharField(max_length=2)
-    email = models.EmailField()
+    email = models.EmailField(max_length=255, null=True, blank=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
 
     def __str__(self):
